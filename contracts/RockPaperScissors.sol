1ff939d296449ef13f210cb7b3baa4fcd45e5f29
chxash <chxash@users.noreply.github.com>
2021-09-28 16:27:09 +0200

    Basic game is working

---
+++ contracts/RockPaperScissors.sol
@@ -0,0 +1,177 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+// Workflow for a user is :
+// 1. Deposit to the contract
+// 2. Call startGame() method
+// 3. Call submitMove() method
+// 4. Call punish() when facing an uncooperative opponent
+// 5. Withdraw or play again
+contract RockPaperScissors {
+    uint public constant requiredDeposit = 100;
+    string public constant MoveRock = "ROCK";
+    string public constant MovePaper = "PAPER";
+    string public constant MoveScissors = "SCISSORS";
+
+    enum State {IDLE, ENROLLED, MOVE_SUBMITTED}
+
+    event PlayerEnrolled(address indexed player);
+
+    struct PlayerInfo {
+        State state;
+        address opponent;
+        string move;
+        uint timeout;
+    }
+
+    address latestAvailableOpponentAddress;
+
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balances;
+
+    /* Array with all players information */
+    mapping (address => PlayerInfo) internal playersInfo;
+
+    constructor() {
+        console.log("Deploying Rock Paper Scissors game");
+    }
+
+    receive() external payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    // prevent withdrawal if in game
+    function withdraw(uint256 amount) external {
+        require(amount <= balances[msg.sender], "Insufficient funds");
+        balances[msg.sender]-=amount;
+        payable(msg.sender).transfer(amount);
+    }
+
+    // Method to be called in order to 
+    function startGame() external {
+        require(requiredDeposit <= balances[msg.sender], "Minimum balance is required");
+        require(playersInfo[msg.sender].state != State.ENROLLED, "Player is already enrolled");
+
+        playersInfo[msg.sender].state = State.ENROLLED;
+
+        if (latestAvailableOpponentAddress != address(0)) {
+            playersInfo[msg.sender].opponent = latestAvailableOpponentAddress;
+            playersInfo[latestAvailableOpponentAddress].opponent = msg.sender;
+            latestAvailableOpponentAddress = address(0);
+        } else {
+            latestAvailableOpponentAddress = msg.sender;
+        }
+
+        emit PlayerEnrolled(msg.sender);
+    }
+
+    // Hash move pour cacher ?
+    // Gas cost
+
+    function submitMove(string memory move) public {
+        // checking if player already paid required tokens
+        require(requiredDeposit <= balances[msg.sender], "Minimum balance is required");
+        require(playersInfo[msg.sender].state == State.ENROLLED, "Player is not enrolled yet");
+
+        //checking if 2 players are enrolled
+        require(playersInfo[msg.sender].opponent != address(0), "No opponent is enrolled yet");
+
+        // discard invalid moves
+        require(compareStrings(move, MoveRock) || compareStrings(move, MovePaper) || compareStrings(move, MoveScissors), "Submitted move is invalid");
+
+        playersInfo[msg.sender].state = State.MOVE_SUBMITTED;
+
+        playersInfo[msg.sender].move = move;
+
+        // storage or memory ?
+        PlayerInfo storage opponent = playersInfo[playersInfo[msg.sender].opponent];
+        //checking if opponent already played => resolution
+        if (opponent.state == State.MOVE_SUBMITTED) {
+            // Equality
+            if (compareStrings(playersInfo[msg.sender].move, opponent.move)) {
+                resolveTie();
+            }
+            else if (compareStrings(playersInfo[msg.sender].move, MoveRock)) {
+                // ROCK > SCISSORS
+                if (compareStrings(opponent.move, MoveScissors)) {
+                    resolveWin();
+                }
+
+                // ROCK < PAPER
+                if (compareStrings(opponent.move, MovePaper)) {
+                    resolveLose();
+                }
+            }
+            else if (compareStrings(playersInfo[msg.sender].move, MovePaper)) {
+                // PAPER > ROCK
+                if (compareStrings(opponent.move, MoveRock)) {
+                    resolveWin();
+                }
+
+                // PAPER < SCISSORS
+                if (compareStrings(opponent.move, MoveScissors)) {
+                    resolveLose();
+                }
+            }
+            else {
+                // SCISSORS > PAPER
+                if (compareStrings(opponent.move, MovePaper)) {
+                    resolveWin();
+                }
+
+                // SCISSORS < ROCK
+                if (compareStrings(opponent.move, MoveRock)) {
+                    resolveLose();
+                }
+            }
+        } else {
+            // start timer for opponent to play
+            opponent.timeout = block.timestamp + 1 days;
+        }
+    }
+
+    // Method to be called to retrieve opponent's token after timeout is elapsed (1 day timeout)
+    function punish() public {
+        uint256 timeout = playersInfo[playersInfo[msg.sender].opponent].timeout;
+        if (timeout != 0 && block.timestamp > timeout) {
+            resolveWin();
+        }
+    }
+
+    function resolveWin() internal {
+        console.log("Hero won");
+        balances[playersInfo[msg.sender].opponent]-=requiredDeposit;
+        balances[msg.sender]+=requiredDeposit;
+        resetPlayersInfo();
+    }
+
+    function resolveLose() internal {
+        console.log("Hero lost");
+        balances[playersInfo[msg.sender].opponent]+=requiredDeposit;
+        balances[msg.sender]-=requiredDeposit;
+        resetPlayersInfo();
+    }
+
+    function resolveTie() internal {
+        console.log("it's a tie");
+        resetPlayersInfo();
+    }
+
+    function resetPlayersInfo() internal {
+        playersInfo[msg.sender].state = State.IDLE;
+        playersInfo[msg.sender].opponent = address(0);
+        playersInfo[msg.sender].move = "";
+        playersInfo[msg.sender].timeout = 0;
+        playersInfo[playersInfo[msg.sender].opponent].state = State.IDLE;
+        playersInfo[playersInfo[msg.sender].opponent].opponent = address(0);
+        playersInfo[playersInfo[msg.sender].opponent].move = "";
+        playersInfo[playersInfo[msg.sender].opponent].timeout = 0;
+    }
+
+    function compareStrings(string memory a, string memory b) internal pure returns (bool) {
+        bool result = (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+        return result;
+    }
+}
---
+++ test/RockerPaperScissors.test.js
@@ -0,0 +1,186 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("RockPaperScissors", function () {
+
+  before(async function () {
+    this.signers = await ethers.getSigners();
+    this.alice = this.signers[0];
+    this.bob = this.signers[1];
+    this.provider = ethers.provider;
+  });
+
+  beforeEach(async function () {
+    this.RockPaperScissorsFactory = await ethers.getContractFactory("RockPaperScissors");
+    this.contract = await this.RockPaperScissorsFactory.deploy();
+    await this.contract.deployed();
+    await this.contract.connect(this.alice);
+  });
+
+  describe("Public methods tests", function () {
+    describe("receive() and withdraw() methods", function () {
+      it("Should increments user balance when receiving funds", async function () {
+        await this.alice.sendTransaction({ to: this.contract.address, value: 100 });
+        await this.bob.sendTransaction({ to: this.contract.address, value: 102 });
+        expect(await this.contract.balances(this.alice.address)).to.equal(100);
+        expect(await this.contract.balances(this.bob.address)).to.equal(102);
+      });
+
+      it("Should withdraw requested users fund when calling withdraw() with less than user balance", async function () {
+        await this.alice.sendTransaction({ to: this.contract.address, value: 100 });
+        await this.bob.sendTransaction({ to: this.contract.address, value: 102 });
+        await expect(() => this.contract.withdraw(99)).to.changeEtherBalances([this.alice, this.contract], [99, -99]);
+        expect(await this.contract.balances(this.alice.address)).to.equal(1);
+        expect(await this.contract.balances(this.bob.address)).to.equal(102);
+      });
+
+      it("Should not withdraw requested users fund when calling withdraw() with more than user balance", async function () {
+        await this.alice.sendTransaction({ to: this.contract.address, value: 1 });
+        await expect(this.contract.withdraw(2)).to.be.revertedWith('Insufficient funds');
+        expect(await this.contract.balances(this.alice.address)).to.equal(1);
+      });
+    });
+
+    describe("startGame() method", function () {
+      it("Should not enroll player if balance is less than required deposit", async function () {
+        await this.alice.sendTransaction({ to: this.contract.address, value: 99 });
+        await expect(this.contract.startGame()).to.be.revertedWith('Minimum balance is required');
+      });
+
+      it("Should enroll player if balance is more than required deposit and no opponent is available", async function () {
+        await this.bob.sendTransaction({ to: this.contract.address, value: 100 });
+        await expect(this.contract.connect(this.bob).startGame()).to.emit(this.contract, 'PlayerEnrolled').withArgs(this.bob.address);
+      });
+
+      it("Should not enroll player which is already enrolled", async function () {
+        await this.bob.sendTransaction({ to: this.contract.address, value: 101 });
+        await expect(this.contract.connect(this.bob).startGame());
+        await expect(this.contract.connect(this.bob).startGame()).to.be.revertedWith('Player is already enrolled');
+      });
+    });
+
+    describe("submitMove() method", function () {
+      it("Should not proceed if balance is less than required deposit", async function () {
+        await this.alice.sendTransaction({ to: this.contract.address, value: 99 });
+        await expect(this.contract.submitMove(this.contract.MoveRock)).to.be.revertedWith('Minimum balance is required');
+      });
+
+      it("Should not proceed if game was not started by user", async function () {
+        await this.alice.sendTransaction({ to: this.contract.address, value: 101 });
+        await expect(this.contract.submitMove(this.contract.MoveRock)).to.be.revertedWith('Player is not enrolled yet');
+      });
+
+      it("Should not proceed if no opponent has been matched", async function () {
+        await this.alice.sendTransaction({ to: this.contract.address, value: 101 });
+        await this.contract.startGame();
+        await expect(this.contract.submitMove(this.contract.MoveRock)).to.be.revertedWith('No opponent is enrolled yet');
+      });
+
+      it("Should not proceed if move is not recognized", async function () {
+        await this.alice.sendTransaction({ to: this.contract.address, value: 101 });
+        await this.bob.sendTransaction({ to: this.contract.address, value: 100 });
+        await this.contract.startGame();
+        await this.contract.connect(this.bob).startGame();
+        await expect(this.contract.submitMove("")).to.be.revertedWith('Submitted move is invalid');
+      });
+
+      it("Should store move and change state if opponent did not submit its move yet", async function () {
+
+      });
+
+      describe("Game resolution cases", function () {
+        beforeEach(async function () {
+          await this.alice.sendTransaction({ to: this.contract.address, value: 102 });
+          await this.bob.sendTransaction({ to: this.contract.address, value: 103 });
+          await this.contract.startGame();
+          await this.contract.connect(this.bob).startGame();
+        });
+
+        it("Should resolves the game if opponent already submitted its move, game result is a tie", async function () {
+          await this.contract.submitMove("ROCK");
+          await this.contract.connect(this.bob).submitMove("ROCK");
+          expect(await this.contract.balances(this.alice.address)).to.equal(102);
+          expect(await this.contract.balances(this.bob.address)).to.equal(103);
+        });
+
+        it("Should resolves the game if opponent already submitted its move, P1 ROCK > P2 SCISSORS", async function () {
+          await this.contract.submitMove("ROCK");
+          await this.contract.connect(this.bob).submitMove("SCISSORS");
+          expect(await this.contract.balances(this.alice.address)).to.equal(202);
+          expect(await this.contract.balances(this.bob.address)).to.equal(3);
+        });
+
+        it("Should resolves the game if opponent already submitted its move, P1 ROCK < P2 PAPER", async function () {
+          await this.contract.submitMove("ROCK");
+          await this.contract.connect(this.bob).submitMove("PAPER");
+          expect(await this.contract.balances(this.alice.address)).to.equal(2);
+          expect(await this.contract.balances(this.bob.address)).to.equal(203);
+        });
+
+        it("Should resolves the game if opponent already submitted its move, P1 PAPER > P2 ROCK", async function () {
+          await this.contract.submitMove("PAPER");
+          await this.contract.connect(this.bob).submitMove("ROCK");
+          expect(await this.contract.balances(this.alice.address)).to.equal(202);
+          expect(await this.contract.balances(this.bob.address)).to.equal(3);
+        });
+
+        it("Should resolves the game if opponent already submitted its move, P1 PAPER < P2 SCISSORS", async function () {
+          await this.contract.submitMove("PAPER");
+          await this.contract.connect(this.bob).submitMove("SCISSORS");
+          expect(await this.contract.balances(this.alice.address)).to.equal(2);
+          expect(await this.contract.balances(this.bob.address)).to.equal(203);
+        });
+
+        it("Should resolves the game if opponent already submitted its move, P1 SCISSORS > P2 PAPER", async function () {
+          await this.contract.submitMove("SCISSORS");
+          await this.contract.connect(this.bob).submitMove("PAPER");
+          expect(await this.contract.balances(this.alice.address)).to.equal(202);
+          expect(await this.contract.balances(this.bob.address)).to.equal(3);
+        });
+
+        it("Should resolves the game if opponent already submitted its move, P1 SCISSORS < P2 ROCK", async function () {
+          await this.contract.submitMove("SCISSORS");
+          await this.contract.connect(this.bob).submitMove("ROCK");
+          expect(await this.contract.balances(this.alice.address)).to.equal(2);
+          expect(await this.contract.balances(this.bob.address)).to.equal(203);
+        });
+
+        it("should be possible to start a game again after winning", async function () {
+          await this.contract.submitMove("ROCK");
+          await this.contract.connect(this.bob).submitMove("SCISSORS");
+          expect(await this.contract.balances(this.alice.address)).to.equal(202);
+          expect(await this.contract.balances(this.bob.address)).to.equal(3);
+          await expect(this.contract.startGame()).to.not.be.reverted;
+          await expect(this.contract.connect(this.bob).startGame()).to.be.revertedWith('Minimum balance is required');
+        });
+
+        it("should not be possible to punish an uncooperative player before a 1 day delay", async function () {
+          await this.contract.submitMove("ROCK");
+          await this.contract.punish();
+          expect(await this.contract.balances(this.alice.address)).to.equal(102);
+          expect(await this.contract.balances(this.bob.address)).to.equal(103);
+
+          await network.provider.send("evm_increaseTime", [86399]);
+
+          await this.contract.punish();
+          
+          expect(await this.contract.balances(this.alice.address)).to.equal(102);
+          expect(await this.contract.balances(this.bob.address)).to.equal(103);
+        });
+
+        it("should be possible to punish an uncooperative player after a 1 day delay", async function () {
+          await this.contract.submitMove("ROCK");
+          await this.contract.punish();
+          expect(await this.contract.balances(this.alice.address)).to.equal(102);
+          expect(await this.contract.balances(this.bob.address)).to.equal(103);
+          await network.provider.send("evm_increaseTime", [86400]);
+
+          await this.contract.punish();
+          
+          expect(await this.contract.balances(this.alice.address)).to.equal(202);
+          expect(await this.contract.balances(this.bob.address)).to.equal(3);
+        });
+      });
+    });
+  });
+});
